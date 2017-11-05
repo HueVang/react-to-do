af84096ed142bd8665b070d0d51587d5841bc4b2
Hue  Vang <huevang@Hues-MacBook-Pro.local>
2017-11-05 02:23:27 -0600

    Added state for input value and constructor in TodoList class.

--- app/components/TodoList.js
+++ app/components/TodoList.js
@@ -1,12 +1,38 @@
 var React = require('react');
 
 class TodoList extends React.Component{
+  constructor(props, context) {
+    super(props, context);
+    this.addItem = this.addItem.bind(this);
+    this.state = {
+      items: []
+    }
+  }
+
+  addItem(e) {
+    var itemArray = this.state.items;
+    if (this._inputElement.value !== "") {
+      itemArray.unshift({
+          text: this._inputElement.value,
+          key: Date.now()
+        });
+        this.setState({
+          items: itemArray
+        });
+        this._inputElement.value = "";
+    }
+    console.log(itemArray);
+    e.preventDefault();
+  }
+
   render() {
     return(
       <div className="todoListMain">
         <div className="header">
-          <form>
-            <input placeholder="enter task">
+          <form onSubmit={this.addItem}>
+            <input
+            ref={(a) => this._inputElement = a}
+            placeholder="enter task">
             </input>
             <button type="submit">Add</button>
           </form>
